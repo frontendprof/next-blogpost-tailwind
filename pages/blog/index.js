412097a3ee5d62e78935bad67a7f3df17de750d4
frontendprof <https://github.com/frontendprof>
2021-09-05 15:13:33 +0500

    404 && about && blog pages

---
+++ pages/blog/index.js
@@ -0,0 +1,11 @@
+import Layout from '../../components/Layout';
+
+const index = () => {
+  return (
+    <Layout>
+      <h1>Here goes all blog posts</h1>
+    </Layout>
+  );
+};
+
+export default index;
